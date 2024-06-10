5edf98c467e213ff959c76a94fd11b459394cbb7
Apolonia Abramowicz <pola.abramowicz@onet.pl>
2024-06-10 23:22:22 +0200

    wave set as background

--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -22,14 +22,7 @@ function WelcomePage() {
                     </div>
                 </div>
             <div className="right-side">
-                <div className={"wave"}>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="832" height="1023" viewBox="0 0 832 1023"
-                         fill="none">
-                        <path
-                            d="M217 849.5C107.4 909.5 27 989.834 0.5 1022.5H832.5V0.500021L774.5 0.5L726.5 72.5C659.885 172.423 583 144.5 424 308C296.8 438.8 303 552.5 322 593C332.667 653.5 326.6 789.5 217 849.5Z"
-                            fill="#4449AE"/>
-                    </svg>
-                </div>
+                <div className={"wave"}> </div>
 
             </div>
         </div>
