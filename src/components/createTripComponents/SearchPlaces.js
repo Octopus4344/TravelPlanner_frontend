d485abf973721f1a601141b59a060edd0276a857
Apolonia Abramowicz <pola.abramowicz@onet.pl>
2024-06-17 11:58:12 +0200

    places with categories

--- src/components/createTripComponents/SearchPlaces.js
+++ src/components/createTripComponents/SearchPlaces.js
@@ -1,27 +1,50 @@
 import React, { useState } from 'react';
-import MapboxAutocomplete from "react-mapbox-autocomplete";
 import axios from "axios";
 
 function SearchPlaces({ destination, onAdd }) {
     const [places, setPlaces] = useState([])
     const [searchValue, setSearchValue] = useState('')
+    const [suggestions, setSuggestions] = useState([])
 
-    const handleSelect = async (result, lat, lng, text) => {
-        const place = {
-            name: text,
-            latitude: lat,
-            longitude: lng,
-            place_name: text,
+    const handleSelect = async (place) => {
+        const selectedPlace = {
+            name: place.text,
+            latitude: place.center[1],
+            longitude: place.center[0],
+            place_name: place.place_name,
+            feature_type: place.place_type[0],
+            category: place.properties?.category || 'Unknown',
+            address: place.properties?.address || "Unknown"
+
         };
 
-            setPlaces([...places, place]);
-            onAdd(place);
-            setSearchValue('')
+        console.log(place);
+        console.log(selectedPlace);
+
+            setPlaces([...places, selectedPlace]);
+            onAdd(selectedPlace);
+            setSearchValue('');
+        setSuggestions([])
 
     };
 
-    const handleSearchChange = (event) => {
-        setSearchValue(event.target.value);
+    const handleSearchChange = async (event) => {
+        const value = event.target.value;
+        setSearchValue(value);
+
+        if (value.length > 2) {
+            const response = await axios.get(`https://api.mapbox.com/geocoding/v5/mapbox.places/${value}.json`,
+                {
+                    params: {
+                        access_token: process.env.REACT_APP_MAPBOX_API_KEY,
+                        proximity: `${destination.longitude},${destination.latitude}`,
+                    },
+                });
+
+            setSuggestions(response.data.features);
+        } else{
+            setSuggestions([]);
+        }
     }
 
 
@@ -29,16 +52,23 @@ function SearchPlaces({ destination, onAdd }) {
     return (
         <div>
             <div className={'login-container'}>
-                <MapboxAutocomplete
-                    publicKey={process.env.REACT_APP_MAPBOX_API_KEY}
-                    inputClass='form-control search'
-                    onSuggestionSelect={handleSelect}
+                <input
+                    type={'text'}
+                    className={'form-control search'}
                     value = {searchValue}
-                    onInput = {handleSearchChange}
-                    placeHolder='Start entering the place you want to visit...'
-                    proximity={`${destination.longitude},${destination.latitude}`}
+                    onChange={handleSearchChange}
+                    placeholder='Start entering the place...'
                 />
             </div>
+                {suggestions.length > 0 && (
+                    <ul className={"suggestion-list"}>
+                        {suggestions.map((suggestion, index) => (
+                            <li key={index} onClick={() => handleSelect(suggestion)}>
+                                {suggestion.place_name}
+                            </li>
+                        ))}
+                    </ul>
+                )}
             <div className={'places-list'}>
                 <ul>
                     {places.map((place, index) => (
